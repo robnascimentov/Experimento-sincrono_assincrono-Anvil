9a3b384f567fdde34b59598a605ef53a26dd86eb
robnascimentov <nviana.rob@gmail.com>
2026-07-02 19:01:56 -0300

    feat: implementado experimento sincrono simulando bloqueio de interface

--- main.py
+++ main.py
@@ -15,3 +15,16 @@ class Form1(Form1Template):
     else:
       self.label_interativo.text = "Botão de teste clicado!"
       self.label_interativo.foreground = "green"
+
+  # 2. Experimento Síncrono (Bloqueia a tela desativando os botões)
+  @handle("btn_sincrono", "click")
+  def btn_sincrono_click(self, **event_args):
+    self.label_status.text = "Processando Áudio (Síncrono)..."
+    self.btn_sincrono.enabled = False
+    self.btn_assincrono.enabled = False
+    self.btn_interativo.enabled = False
+    time.sleep(5)
+    self.btn_sincrono.enabled = True
+    self.btn_assincrono.enabled = True
+    self.btn_interativo.enabled = True
+    self.label_status.text = "Áudio síncrono processado!"
